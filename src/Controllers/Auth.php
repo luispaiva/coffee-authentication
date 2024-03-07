b66e299e3e6fed160124a8cb3ddf4a726b6a4e34
Luis Paiva <contato@luispaiva.com.br>
2024-03-07 00:20:05 -0300

    Adição de método para token de atualização.

--- src/Controllers/Auth.php
+++ src/Controllers/Auth.php
@@ -44,4 +44,35 @@ class Auth {
 
 		return new \WP_REST_Response( Token::generate( $user ), 200 );
 	}
+
+	/**
+	 * Refresh token.
+	 *
+	 * @param \WP_REST_Request $request Request.
+	 *
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public function refresh( \WP_REST_Request $request ) {
+
+		if ( empty( $request->get_param( 'refresh_token' ) ) ) {
+			return new \WP_Error(
+				'required_params',
+				esc_html__( 'O refresh token é obrigatório!', 'coffee' ),
+				array( 'status' => 401 )
+			);
+		}
+
+		$refreshToken = $request->get_param( 'refresh_token' );
+		$user         = Token::validateRefreshToken( $refreshToken );
+
+		if ( is_wp_error( $user ) ) {
+			return new \WP_Error(
+				'invalid_refresh_token',
+				esc_html__( 'Refresh token inválido!', 'coffee' ),
+				array( 'status' => 401 )
+			);
+		}
+
+		return new \WP_REST_Response( Token::generate( $user ), 200 );
+	}
 }
