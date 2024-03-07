523a6d467edfd06a7b3224162db30533b1ebfa8a
Luis Paiva <contato@luispaiva.com.br>
2024-03-07 00:26:48 -0300

    Adicionado métodos para setar e validar o refresh toke.

--- src/Controllers/Token.php
+++ src/Controllers/Token.php
@@ -48,9 +48,8 @@ class Token {
 
 		$issued_at  = time();
 		$not_before = apply_filters( 'jwt_auth_not_before', $issued_at, $issued_at );
-		$expire     = apply_filters( 'jwt_auth_token_expire', $issued_at + ( DAY_IN_SECONDS * 7 ) );
-
-		$token = array(
+		$expire     = apply_filters( 'jwt_auth_token_expire', $issued_at + ( MINUTE_IN_SECONDS * 60 ) );
+		$token      = array(
 			'iss'  => get_bloginfo( 'url' ),
 			'iat'  => $issued_at,
 			'nbf'  => $not_before,
@@ -62,13 +61,17 @@ class Token {
 			),
 		);
 
-		$token = JWT::encode( $token, self::$secret_key, 'HS256' );
+		$token        = JWT::encode( $token, self::$secret_key, 'HS256' );
+		$refreshToken = wp_generate_uuid4();
+
+		self::setRefreshToken( $user->data->ID, $refreshToken );
 
 		$data = array(
-			'token' => $token,
-			'id'    => $user->data->ID,
-			'name'  => $user->data->display_name,
-			'email' => $user->data->user_email,
+			'token'        => $token,
+			'refreshToken' => $refreshToken,
+			'id'           => $user->data->ID,
+			'name'         => $user->data->display_name,
+			'email'        => $user->data->user_email,
 		);
 
 		return apply_filters( 'jwt_auth_token_before_dispatch', $data, $user );
@@ -87,5 +90,67 @@ class Token {
 		} catch ( \Exception $e ) {
 			return new \WP_Error( 'rest_auth_invalid_token', $e->getMessage(), array( 'status' => 403 ) );
 		}
+	}
+
+	/**
+	 * Validate refresh token.
+	 *
+	 * @param string $refreshToken Refresh token.
+	 *
+	 * @return \WP_User|WP_Error
+	 */
+	public static function validateRefreshToken( string $refreshToken ) {
+		$userId = self::getUserIdByRefreshToken( $refreshToken );
+
+		if ( ! $userId ) {
+			return new \WP_Error(
+				'rest_auth_invalid_refresh_token',
+				esc_html__( 'Refresh token inválido!', 'coffee' ),
+				array( 'status' => 403 )
+			);
+		}
+
+		return get_user_by( 'id', $userId );
+	}
+
+	/**
+	 * Refresh token.
+	 *
+	 * @param int    $userId       User ID.
+	 * @param string $refreshToken Refresh token.
+	 *
+	 * @return bool
+	 */
+	private static function setRefreshToken( $userId, $refreshToken ) {
+		return update_user_meta( $userId, 'refresh_token', $refreshToken );
+	}
+
+	/**
+	 * Get user ID by refresh token.
+	 *
+	 * @param string $refreshToken Refresh token.
+	 *
+	 * @return int
+	 */
+	private static function getUserIdByRefreshToken( $refreshToken ) {
+		global $wpdb;
+
+		$cache_key = 'refresh_token_' . $refreshToken;
+		$userId    = wp_cache_get( $cache_key, 'refresh_token' );
+
+		if ( false !== $userId ) {
+			return (int) $userId;
+		}
+
+		$userId = $wpdb->get_var( //phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery
+			$wpdb->prepare(
+				"SELECT user_id FROM $wpdb->usermeta WHERE meta_key = 'refresh_token' AND meta_value = %s",
+				$refreshToken
+			)
+		);
+
+		wp_cache_set( $cache_key, $userId, 'refresh_token' );
+
+		return (int) $userId;
 	}
 }
