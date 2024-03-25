f34bdfa995715c2b39b78f80838210fd26dcf161
Luis Paiva <contato@luispaiva.com.br>
2024-03-25 00:41:11 -0300

    Adicionado estrutura para trabalhar com PHPUnit.

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Bootstrap file for the plugin tests.
+ *
+ * @package Coffee
+ */
+
+// Load the Composer autoloader.
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Load WordPress.
+require_once __DIR__ . '/../../../../wp-load.php';
